32bb115878cecd143a8270f086565d59c4f067f1
HeesikK <7jjhkhz@naver.com>
2023-10-16 15:03:00 +0900

    style: header css 적용

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,5 +1,65 @@
+import styled from "styled-components";
+import { flexAlignCenter, flexCenter } from "../../styles/common.style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <S.Wrapper>
+      <S.Navbar>
+        <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" width={150} />
+        <S.FilterList>
+          <li>Now Playing</li>
+          <li>Popular</li>
+          <li>Top Rated</li>
+          <li>Upcoming</li>
+        </S.FilterList>
+      </S.Navbar>
+      <div>
+        <FontAwesomeIcon icon={faMagnifyingGlass} style={{ position: "relative", left: 18 }} />
+        <S.MovieInput type="text" />
+      </div>
+    </S.Wrapper>
+  );
 };
 
 export default Header;
+
+const Wrapper = styled.div`
+  justify-content: space-between;
+  ${flexAlignCenter};
+  padding: 10px;
+  margin-left: 50px;
+  margin-right: 50px;
+`;
+
+const Navbar = styled.div`
+  ${flexCenter}
+`;
+
+const FilterList = styled.ul`
+  ${flexCenter}
+  > li {
+    padding: 15px;
+    cursor: pointer;
+    &:hover {
+      color: red;
+    }
+  }
+`;
+
+const MovieInput = styled.input`
+  padding: 10px 10px 10px 25px;
+  border: none;
+  border-bottom: 2px solid black;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const S = {
+  Wrapper,
+  Navbar,
+  FilterList,
+  MovieInput,
+};
