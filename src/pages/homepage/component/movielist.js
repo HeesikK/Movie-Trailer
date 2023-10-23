2e1fb33e1d76b403d3d3375e6ebd0bce1e91c55d
HeesikK <7jjhkhz@naver.com>
2023-10-23 10:29:05 +0900

    style: css 수정

--- src/pages/homepage/component/movielist.js
+++ src/pages/homepage/component/movielist.js
@@ -31,7 +31,7 @@ const MovieList = () => {
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollTop = document.documentElement.scrollTop;
     const clientHeight = document.documentElement.clientHeight;
-    if (scrollTop + clientHeight >= scrollHeight - 445) return fetchNextPage();
+    if (scrollTop + clientHeight >= scrollHeight) return fetchNextPage();
   };
 
   useEffect(() => {
